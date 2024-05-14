3f6a61ab5479ad15fa8cc2625831b5d9f5281fb6
Jimisola Laursen <jimisola.laursen@lfv.se>
2024-05-14 16:14:37 +0200

    feat: Initial implementation

---
+++ src/test/java/se/lfv/maven/plugins/CIFriendlyVersionsPluginTests.java
@@ -0,0 +1,14 @@
+// Copyright © LFV
+package se.lfv.maven.plugins;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class CIFriendlyVersionsPluginTests {
+
+	@Test
+	void testNothing() throws IOException {
+	}
+
+}
